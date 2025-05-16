be6deb05261ce861fa77c6fdcca1a6bf9e1f356c
StephenAraka <stevenaraka@gmail.com>
2025-05-16 13:53:23 +0300

    Fetch journals from db

--- app/(root)/(tabs)/journals.tsx
+++ app/(root)/(tabs)/journals.tsx
@@ -1,70 +1,62 @@
 import ScreenTitleHeader from '@/components/ScreenTitleHeader';
-import { moods } from '@/constants';
+import { images } from '@/constants';
+import { api } from '@/convex/_generated/api';
+import { getCoverImage } from '@/lib/utils/helpers';
+import { Journal } from '@/types/type';
+import { useUser } from '@clerk/clerk-expo';
+import { useQuery } from 'convex/react';
 import { useRouter } from 'expo-router';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const mockJournals = [
-  {
-    id: '1',
-    title: 'A Beautiful Day',
-    mood: moods[0],
-    date: 'May 15, 2025',
-    description: 'Today I felt really great and full of energy. I went for a long walk and enjoyed the sunshine. I also met up with some friends for lunch, which was really nice. I love days like this!',
-  },
-  {
-    id: '2',
-    title: 'Feeling Blue',
-    mood: moods[1],
-    date: 'May 14, 2025',
-    description: 'It was a rough day. I felt pretty low emotionally  and struggled to get out of bed. I spent most of the day watching TV and eating junk food. I hope tomorrow is better. I need to take care of myself.',
-  },
-  {
-    id: '3',
-    title: 'Finding Calm',
-    mood: moods[3],
-    date: 'May 13, 2025',
-    description: 'I took some time to meditate and practice mindfulness today. It really helped me feel more centered and calm. I also did some yoga, which was a great way to release tension.',
-  },
-  {
-    id: '4',
-    title: 'Anxious Thoughts',
-    mood: moods[2],
-    date: 'May 12, 2025',
-    description: 'I had a lot of anxious thoughts today. I felt overwhelmed by everything going on in my life. I tried to talk to a friend about it, but it was hard to express how I was feeling.',
-  },
-];
-
 const Journals = () => {
-  const router = useRouter();
+  const { user } = useUser()
+  const userEmail = user?.emailAddresses[0].emailAddress || '';
 
-  const renderItem = ({ item }: { item: typeof mockJournals[0] }) => (
+  const router = useRouter();
+  const journals = useQuery(api.journals.get, {
+    owner: userEmail,
+  }) || [];
+
+
+  const renderItem = ({ item }: { item: Journal }) => (
     <TouchableOpacity
       className="flex-row items-center bg-white rounded-lg shadow-sm p-4 mb-3 mx-4"
       onPress={() => router.push(
         {
           pathname: "/journal-detail",
-          params: item,
+          params: { ...item },
         }
       )}
     >
-      <Image source={item.mood} className="w-16 h-16 rounded-md mr-4" resizeMode="cover" />
+      <Image source={getCoverImage(item.mood)} className="w-16 h-16 rounded-md mr-4" resizeMode="cover" />
       <View className="flex-1">
         <Text className="text-base font-semibold text-gray-800">{item.title}</Text>
-        <Text className="text-sm text-gray-500">{item.date}</Text>
+        <Text className="text-sm text-gray-500">{item.createdAt}</Text>
       </View>
     </TouchableOpacity>
   );
 
   return (
     <SafeAreaView className="flex-1 bg-gray-50 pt-4">
-    <ScreenTitleHeader showBackButton={false} title="My Journals" />
+      <ScreenTitleHeader showBackButton={false} title="My Journals" />
+      {journals.length === 0 ? (
+        <View className="flex-1 items-center justify-center">
+          <Image
+            source={images.noResult}
+            className="w-32 h-32 mb-4"
+            resizeMode="contain"
+          />
+          <Text className="text-gray-500 text-lg">No journals found.</Text>
+        </View>
+      ) : (
       <FlatList
-        data={mockJournals}
-        keyExtractor={(item) => item.id}
+        data={journals}
+        keyExtractor={(item) => item._id}
         renderItem={renderItem}
         contentContainerStyle={{ paddingBottom: 16 }}
       />
+    )}
     </SafeAreaView>
   );
 };
