beb52f8cd29d67146c329b769cdad90a4b82544a
StephenAraka <stevenaraka@gmail.com>
2025-05-16 09:22:42 +0300

    Create profile screen

--- constants/index.ts
+++ constants/index.ts
@@ -6,6 +6,7 @@ import eyecross from "@/assets/icons/eyecross.png";
 import google from "@/assets/icons/google.png";
 import home from "@/assets/icons/home.png";
 import list from "@/assets/icons/list.png";
+import plus from "@/assets/icons/plus.png";
 import profile from "@/assets/icons/profile.png";
 import check from "@/assets/images/check.png";
 import anxious from "@/assets/images/moods/anxious.png";
@@ -35,5 +36,34 @@ export const icons = {
   google,
   home,
   list,
+  plus,
   profile,
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "The perfect ride is just a tap away!",
+    description:
+      "Your journey begins with Ryde. Find your ideal ride effortlessly.",
+    image: images.onboarding1,
+  },
+  {
+    id: 2,
+    title: "Best car in your hands with Ryde",
+    description:
+      "Discover the convenience of finding your perfect ride with Ryde",
+    image: images.onboarding2,
+  },
+  {
+    id: 3,
+    title: "Your ride, your way. Let's go!",
+    description:
+      "Enter your destination, sit back, and let us take care of the rest.",
+    image: images.onboarding3,
+  },
+];
+
+export const data = {
+  onboarding,
 };
