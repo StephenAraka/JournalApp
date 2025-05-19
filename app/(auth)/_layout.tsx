7ee6ffa572c1a6b22733c0d2f0eeb974b94620a2
StephenAraka <stevenaraka@gmail.com>
2025-05-19 21:25:30 +0300

    Remove extra statusbar

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -4,8 +4,8 @@ const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false, statusBarStyle: 'dark' }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false, statusBarStyle: 'dark' }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from "expo-router";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false, statusBarStyle: 'dark' }} />
-      <Stack.Screen name="add-journal" options={{ headerShown: false, statusBarStyle: 'dark' }} />
-      <Stack.Screen name="journal-detail" options={{ headerShown: false, statusBarStyle: 'dark' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="add-journal" options={{ headerShown: false }} />
+      <Stack.Screen name="journal-detail" options={{ headerShown: false }} />
     </Stack>
   );
 };
